e11c331007d653fb75d3991f13b65fc2365e362d
Samster91 <samster91>
2022-07-01 18:31:13 +0200

    Updates

--- precache-manifest.38639fea3b60d79bfcb164d29ced84c5.js
+++ precache-manifest.38639fea3b60d79bfcb164d29ced84c5.js
@@ -12,16 +12,16 @@ self.__precacheManifest = [
     "url": "/static/js/1.9f271624.chunk.js"
   },
   {
-    "revision": "a0fb9c9d05b62729e4a0",
-    "url": "/static/js/7.a0fb9c9d.chunk.js"
+    "revision": "23d374b502c01fe2913c",
+    "url": "/static/js/7.23d374b5.chunk.js"
   },
   {
     "revision": "3db509e53468f9b88899",
     "url": "/static/js/main.3db509e5.chunk.js"
   },
   {
-    "revision": "03a02d16d4bb035ed07f",
-    "url": "/static/js/runtime~main.03a02d16.js"
+    "revision": "61159bffcffde7a7b838",
+    "url": "/static/js/runtime~main.61159bff.js"
   },
   {
     "revision": "48d2503bd1084634e938",
@@ -40,7 +40,7 @@ self.__precacheManifest = [
     "url": "/static/css/main.4eecac7a.chunk.css"
   },
   {
-    "revision": "a0fb9c9d05b62729e4a0",
+    "revision": "23d374b502c01fe2913c",
     "url": "/static/css/7.6f592429.chunk.css"
   },
   {
@@ -52,7 +52,7 @@ self.__precacheManifest = [
     "url": "/static/css/1.306af952.chunk.css"
   },
   {
-    "revision": "5ac4e425690b21ce97332597bd991b39",
+    "revision": "8ed94a2dda4d5c28fe3e637693b782ab",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1694b6148cf3e7f6c14736d90873989a.js"
+  "/precache-manifest.38639fea3b60d79bfcb164d29ced84c5.js"
 );
 
 workbox.clientsClaim();
